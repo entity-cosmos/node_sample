d2c920c7ed2e022ae9f4076c50269af92165689b
Srajan B Shetty <srajanbshetty@gmail.com>
2023-11-21 18:14:18 +0530

    Add SQLite database and CRUD routes

--- server.js
+++ server.js
@@ -1,18 +1,61 @@
-//sample express
 const express = require('express');
-//import body parser
-const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-//get number from body and diplay that value using body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.post('/number', (req, res) => {
-    const num1 = req.body.num1;
-    const num2 = req.body.num2;
-  res.send(`The sum of ${num1} and ${num2} is ${num1 + num2}`);
+// Create SQLite database and table
+const db = new sqlite3.Database('./data.db');
+db.run('CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, note TEXT)');
+
+app.use(express.json());
+
+// CRUD routes
+
+// Create
+app.post('/notes', (req, res) => {
+    const { note } = req.body;
+    db.run('INSERT INTO notes (note) VALUES (?)', [note], function (err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ id: this.lastID, note });
+    });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Read
+app.get('/notes', (req, res) => {
+    db.all(`SELECT * FROM notes `, (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(rows);
+    });
+});
+
+// Update
+app.put('/notes/:id', (req, res) => {
+    const { note } = req.body;
+    const { id } = req.params;
+    db.run('UPDATE notes SET note = ? WHERE id = ?', [note, id], (err) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ id, name });
+    });
+});
+
+// Delete
+app.delete('/notes/:id', (req, res) => {
+    const { id } = req.params;
+    db.run('DELETE FROM notes WHERE id = ?', [id], (err) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ message: 'Item deleted', id });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
